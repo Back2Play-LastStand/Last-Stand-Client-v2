c40a7ae7235b874aab03025486cae2762e9905d8
hyuli0520 <minhyeogjeon945@gmail.com>
2025-06-09 14:11:47 +0900

    Create UI_Lobby.cs

---
+++ Assets/Scripts/UI/Scene/UI_Lobby.cs
@@ -0,0 +1,61 @@
+using Protocol;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Lobby : UI_Scene
+{
+    enum GameObjects
+    {
+        RoomNameInput,
+        CreateButton,
+        JoinButton,
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+        Bind<GameObject>(typeof(GameObjects));
+
+        GetObject((int)GameObjects.CreateButton).AddUIEvent((PointerEventData) => { CreateGame(); });
+        GetObject((int)GameObjects.JoinButton).AddUIEvent((PointerEventData) => { JoinGame(); });
+    }
+
+    public string RoomName { get; protected set; }
+
+    public void CreateGame()
+    {
+        InputField input = GetObject((int)GameObjects.RoomNameInput).GetComponent<InputField>();
+        if (input.text == null)
+        {
+            Debug.Log("Room Name is null");
+            return;
+        }
+        else
+        {
+            Debug.Log("CreateGame!!");
+            RoomName = input.text;
+            JoinRoom();
+        }
+    }
+    public void JoinGame()
+    {
+        InputField input = GetObject((int)GameObjects.RoomNameInput).GetComponent<InputField>();
+        if (input.text == null)
+        {
+            Debug.Log("Room Name is null");
+            return;
+        }
+        else
+        {
+            Debug.Log("JoinGame!!");
+            RoomName = input.text;
+            JoinRoom();
+        }
+    }
+    public void JoinRoom()
+    {
+    }
+}
