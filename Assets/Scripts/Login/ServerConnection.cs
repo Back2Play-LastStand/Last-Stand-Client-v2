441d7edad02d51293d5459c62d4969891840d739
hyuli0520 <minhyeogjeon945@gmail.com>
2025-06-23 09:59:40 +0900

    fix: VerifyCode

--- Assets/Scripts/Login/ServerConnection.cs
+++ Assets/Scripts/Login/ServerConnection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 [CreateAssetMenu(fileName = "ServerConnection", menuName = "Configs/ServerConnection")]
 public class ServerConnection : ScriptableObject
@@ -10,12 +11,13 @@ public class ServerConnection : ScriptableObject
     public string RegisterEndpoint = "/auth/register";
     public string LoginEndpoint = "/auth/login";
     public string VerifyCodeEndpoint = "/verify/send";
+    public string VerifyEndpoint = "/verify";
 
     public string RegisterUrl => BaseUrl + RegisterEndpoint;
     public string LoginUrl => BaseUrl + LoginEndpoint;
-    public string VerifyCodeUrl => BaseUrl + VerifyCodeEndpoint;
+    public string VerifyCodeUrl => BaseUrl + VerifyEndpoint;
 
-    public string GetVerifyEmailUrl(string email) => $"{BaseUrl}/verify/send/email?email={UnityEngine.Networking.UnityWebRequest.EscapeURL(email)}";
+    public string GetVerifyEmailUrl(string email) => $"{BaseUrl}/verify/send?email={UnityWebRequest.EscapeURL(email)}";
     public string GetTopRankingEndpoint(int count) => $"/ranking/top/{count}";
     public string GetTopRankingUrl(int count) => BaseUrl + GetTopRankingEndpoint(count);
     public string GetPlayerRankingUrl(string playerId) => $"{BaseUrl}/ranking/{playerId}";
