d2b4996b9d354fc966d7d0841c910a1fe9fd14e7
hyuli0520 <minhyeogjeon945@gmail.com>
2025-06-27 14:17:57 +0900

    Update UI_CreateRoom.cs

--- Assets/Scripts/UI/Popup/UI_CreateRoom.cs
+++ Assets/Scripts/UI/Popup/UI_CreateRoom.cs
@@ -1,3 +1,4 @@
+using Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,17 +24,28 @@ public class UI_CreateRoom : UI_Popup
         Bind<GameObject>(typeof(GameObjects));
 
         GetObject((int)GameObjects.RoomPanel).GetComponentInChildren<Text>().text = Managers.UI.m_lobby.RoomName;
-        GetObject((int)GameObjects.Player1Panel).GetComponentInChildren<Text>().text = Managers.UI.m_inputName.name;
-        GetObject((int)GameObjects.Player2Panel).gameObject.SetActive(false);
-        GetObject((int)GameObjects.Player3Panel).gameObject.SetActive(false);
-        GetObject((int)GameObjects.Player4Panel).gameObject.SetActive(false);
         GetObject((int)GameObjects.StartGame).AddUIEvent((PointerEventData) => { StartGame(); });
         GetObject((int)GameObjects.ExitRoom).AddUIEvent((PointerEventData) => { ClosePopupUI(); });
     }
+
+    [field: SerializeField]
+    public GameObject[] PlayerSlots = new GameObject[4];
 
     void StartGame()
     {
         var lobby = Managers.Scene.CurrentScene.GetComponent<LobbyScene>();
         lobby.TurnGameScene();
+    }
+
+    public void SetPlayer(RES_ENTER_GAMEROOM enter)
+    {
+        foreach (var slot in PlayerSlots)
+            slot.SetActive(false);
+
+        for (int i = 0; i < enter.Players.Count && i < PlayerSlots.Length; i++)
+        {
+            PlayerSlots[i].SetActive(true);
+            PlayerSlots[i].GetComponentInChildren<Text>().text = enter.Players[i].Name;
+        }
     }
 }
